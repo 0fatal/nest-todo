32ae65a06433d153327a527eb7ccc31cc1be5f02
ZacharyWin <2816813070@qq.com>
2021-08-30 23:19:38 +0800

    fect: cors

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,16 @@ import { ValidationPipe } from '@nestjs/common'
 import { HttpExceptionFilter } from './error/http-exception.filter'
 import { AllExceptionFilter } from './error/all-exception.filter'
 import { TransformInterceptor } from './transform/transform.interceptor'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: {
+      origin: ['http://localhost:8080'],
+      credentials: true,
+    },
+    bufferLogs: true,
+  })
 
   const options = new DocumentBuilder()
     .setTitle('Cats example')
